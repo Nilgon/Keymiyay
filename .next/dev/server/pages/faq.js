fe4ba48ba934276b226d1e80d24c1629602cd518
Nilgon <nilofarr.gh13@gmail.com>
2026-07-11 18:58:10 +0330

    update project

--- .next/dev/server/pages/faq.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/faq.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0y5pss9._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1ttytvy._.js")
-R.c("server/chunks/ssr/node_modules_1_aarlg._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__183ev-w._.js")
-R.c("server/chunks/ssr/pages__app_jsx_05ih_-d._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/faq.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.jsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.jsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/faq.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.jsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.jsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
